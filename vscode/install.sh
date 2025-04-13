3ce43ca8e5c09ec4caa59d4ec2d0e2331eb51078
Sylvain Gaunet <1552102+sgaunet@users.noreply.github.com>
2025-04-13 21:06:47 +0200

    Add VSCode configuration files for extensions, launch settings, and editor preferences

---
+++ vscode/install.sh
@@ -0,0 +1,34 @@
+ #!/usr/bin/env bash
+#code --extensions-dir <dir>
+#    Set the root path for extensions.
+#code --list-extensions
+#    List the installed extensions.
+#code --show-versions
+#    Show versions of installed extensions, when using --list-extension.
+#code --install-extension (<extension-id> | <extension-vsix-path>)
+#    Installs an extension.
+#
+# https://code.visualstudio.com/docs/editor/extension-gallery#_command-line-extension-management
+
+code --install-extension a-h.templ
+code --install-extension aaron-bond.better-comments
+code --install-extension CoenraadS.bracket-pair-colorizer-2
+code --install-extension golang.go
+code --install-extension yzhang.markdown-all-in-one
+code --install-extension docker.docker
+code --install-extension github.copilot
+code --install-extension github.copilot-chat
+code --install-extension github.vscode-github-actions
+code --install-extension golang.go
+code --install-extension hashicorp.terraform
+code --install-extension mhutchie.git-graph
+code --install-extension ms-azuretools.vscode-docker
+code --install-extension ms-vscode-remote.remote-containers
+code --install-extension alefragnani.Bookmarks
+code --install-extension alefragnani.project-manager
+code --install-extension exiasr.hadolint
+code --install-extension yzhang.markdown-all-in-one
+code --install-extension ms-vscode-remote.vscode-remote-extensionpack
+code --install-extension timonwong.shellcheck
+code --install-extension task.vscode-task
+code --install-extension griimick.vhs
